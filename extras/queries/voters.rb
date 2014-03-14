c956f2c090e8370fc82e5289e65c9c48e3545265
Robert Guthrie <rob@guthr.ie>
2014-03-14 14:50:07 +1300

    ok

--- extras/queries/voters.rb
+++ extras/queries/voters.rb
@@ -1,10 +1,5 @@
 class Queries::Voters
   class << self
-    def users_that_voted_on(motion)
-      User.joins(votes: {motion: {discussion: {}}}).
-           where(['motions.id = ?', motion.id])
-    end
-
     def group_members_that_havent_voted_on(motion)
       user_ids_that_voted = User.find motion.votes.pluck(:user_id)
       if user_ids_that_voted.empty?
